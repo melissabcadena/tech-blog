d58569c327764e9fecdbe67740ee643efe1c721d
Melissa Cadena <melissabcadena@Melissas-MBP.attlocal.net>
2020-08-22 17:24:42 -0500

    add login logic

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -55,7 +55,15 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbUserData => {
+        req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+      
+        res.json(dbUserData);
+        });
+      })
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
@@ -74,12 +82,19 @@ router.post('/login', (req, res) => {
       }
   
       const validPassword = dbUserData.checkPassword(req.body.password);
+  
       if (!validPassword) {
         res.status(400).json({ message: 'Incorrect password!' });
         return;
       }
   
-      res.json({ user: dbUserData, message: 'You are now logged in!' });
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+  
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+      });
     });
   });
 
