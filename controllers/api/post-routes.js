4095b2d71596c3905e0bbf966a4d3618aceb4cf9
Melissa Cadena <melissabcadena@Melissas-MBP.attlocal.net>
2020-08-22 09:59:48 -0500

    added comment routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,11 +1,19 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Comment } = require('../../models');
 
 router.get('/', (req, res) => {
     Post.findAll({
         attributes: ['id', 'title', 'blog_content', 'created_at'],
         order: [['created_at', 'DESC']],
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User, 
+                    attributes: ['username']
+                }
+            },
             {
                 model: User, 
                 attributes: ['username']
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -18,9 +18,23 @@ router.get('/:id', (req, res) => {
     User.findOne({
         attributes: { exclude: ['password'] },
         where: {
-            id: req.params.id
-        }
-    })
+          id: req.params.id
+        },
+        include: [
+          {
+            model: Post,
+            attributes: ['id', 'title', 'post_url', 'created_at']
+          },
+          {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'created_at'],
+            include: {
+              model: Post,
+              attributes: ['title']
+            }
+          }
+        ]
+      })
     .then(dbUserData => {
         if (!dbUserData) {
             res.status(404).json({ message: 'No user found with this id' })
