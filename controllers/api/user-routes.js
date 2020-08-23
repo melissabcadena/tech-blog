b13ccd0d7d67f6dd0c9f237c1e7d7a7e25481b95
Melissa Cadena <melissabcadena@Melissas-MBP.attlocal.net>
2020-08-23 15:24:36 -0500

    working on logout

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -98,6 +98,17 @@ router.post('/login', (req, res) => {
     });
   });
 
+  router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+          res.status(204).end();
+        });
+      }
+      else {
+        res.status(404).end();
+      }
+});
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
     User.update(req.body, {
@@ -139,5 +150,7 @@ router.delete('/:id', (req, res) => {
           res.status(500).json(err);
         });
 });
+
+
 
 module.exports = router;
