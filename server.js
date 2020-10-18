0f02d6e759595cbcfc70b3aa67f2d50ff0ff179e
Melissa Cadena <melissabcadena@Melissas-MBP.attlocal.net>
2020-10-17 20:07:02 -0500

    finishing touches

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(require('./controllers/'));
 
 
 // turn on connection to db and server
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
 
