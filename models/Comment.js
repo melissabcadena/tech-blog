3274c6063a17a05d7e73c39d65e418726bcf8e9c
Melissa Cadena <melissabcadena@Melissas-MBP.attlocal.net>
2020-10-17 20:00:25 -0500

    db

--- models/Comment.js
+++ models/Comment.js
@@ -19,7 +19,7 @@ Comment.init(
         type: DataTypes.INTEGER, 
         allowNull: false,
         references: {
-            model: 'user',
+            model: 'User',
             key: 'id'
         }
       },
